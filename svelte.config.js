e96ffdbd88fbaa982437a5a54ef7e0b95b0dbe2a
Mauricio Moraga <mmoraga.m3@gmail.com>
2024-04-17 22:56:11 -0400

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,26 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	},
-  preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	
+	kit: {
+			adapter: adapter({
+				// default options are shown. On some platforms
+				// these options are set automatically — see below
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: true
+			}),
+			paths: {
+				base: dev ? '' : process.env.BASE_PATH
+			}
+		},
 };
 export default config;
